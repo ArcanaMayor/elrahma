f37057ecd31a952581aceaf038912c99cb91d6bb
ArcaneMayor <thomasbang436@gmail.com>
2025-07-14 08:44:27 +0700

    Fitur awal website

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\InfoController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', [AuthController::class,'showLogin'])->name('login');
+Route::post('/login', [AuthController::class,'login'])->name('login');
+Route::post('/logout', [AuthController::class,'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+Route::get('/admin/dashboard', function () {
+return view('admin.dashboard'); // Buat view sederhana untuk dashboard
+})->name('dashboard');
 });
+
+Route::resource('users', App\Http\Controllers\UserController::class);
+Route::resource('kategoris', KategoriController::class);
+Route::resource('infos', InfoController::class);
